ae7349dace51b70df66ea5157e4da729c1f9d577
souvik-pl <souvik.9401@gmail.com>
2023-10-20 22:19:07 +0530

    fix: handled null values for "whiteSpaceValidator" funtion in "ValidatorService"

--- src/app/_services/validator.service.ts
+++ src/app/_services/validator.service.ts
@@ -7,7 +7,7 @@ export class ValidatorService {
   constructor() { }
 
   public whiteSpaceValidator(control: FormControl): { [key: string]: boolean } | null {
-    if (control.value.replace(/\s/g, '').length === 0) {
+    if (control.value?.replace(/\s/g, '').length === 0) {
       return { 'whitespaceError': true };
     }
     return null;
